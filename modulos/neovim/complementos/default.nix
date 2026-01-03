2c9a8cd27dc0c4178e6f105033a4f3b645f60716
anfigeno <dolorcriticodevastador@proton.me>
2026-01-02 19:26:52 -0500

    Cambiamos la lista de nombres por lista de rutas Esto porque ahora si tengo auto completado de rutas, porque descubrí que nunca había añadido la fuente "path" a cmp, aunque tenía instalado el plugin que lo permitía cf

---
+++ modulos/neovim/complementos/default.nix
@@ -0,0 +1,52 @@
+{ pkgs, lib }:
+let
+  rutasDeComplementos = [
+    ./wich-key
+    ./lazydev
+    ./keep-split-ratio
+    ./ts-autotag
+    ./smear-cursor
+    ./tabout
+    ./todo-comments
+    ./inc-rename
+    ./colorizer
+    ./statuscol
+    ./satellite
+    ./gitsigns
+    ./ufo
+    ./otter
+    ./bufferline
+    ./neoformat
+    ./rainbow-delimiters
+    ./autopairs
+    ./telescope
+    ./modes
+    ./lualine
+    ./navic
+    ./hover
+    ./fidget
+    ./treesitter
+    ./mestizo
+    ./cmp
+    ./lspconfig
+    ./fyler
+    ./mini
+    ./noice
+    ./dashboard
+    ./stay-centered
+  ];
+
+  deGithub = import ../util/deGithub.nix { inherit pkgs lib; };
+  formatearDependenciasDeLazy = import ../util/formatearDependenciasDeLazy.nix;
+in
+map (
+  rutaDeComplemento:
+  #lua
+  let
+    complemento = import rutaDeComplemento { inherit pkgs deGithub; };
+    #lua
+  in
+  ''
+    (${complemento.config})("${complemento.paquete}", { ${formatearDependenciasDeLazy complemento.dependencias} })
+  ''
+) rutasDeComplementos
--- modulos/neovim/lazy.nix
+++ modulos/neovim/lazy.nix
@@ -1,6 +1,8 @@
-{ pkgs, lib }: let
-  complementos = import ./complementos.nix { inherit pkgs lib; };
-in {
+{ pkgs, lib }:
+let
+  complementos = import ./complementos { inherit pkgs lib; };
+in
+{
   plugin = pkgs.vimPlugins.lazy-nvim;
   type = "lua";
   config = # lua
