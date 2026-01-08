8e335d7c1662830a632989fcc786ff249023684e
anfigeno <dolorcriticodevastador@proton.me>
2026-01-08 06:35:43 -0500

    Entradas de escritorio para cada perfil de Firefuego

--- modulos/entornoHyprland/firefox/default.nix
+++ modulos/entornoHyprland/firefox/default.nix
@@ -5,14 +5,39 @@
   inputs,
   ...
 }:
+let
+  perfiles = import ./perfiles { inherit lib inputs pkgs; };
+in
 {
-  home-manager.users.${usuario}.programs.firefox = {
-    enable = true;
-    package = (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) { });
-    profiles = import ./perfiles { inherit lib inputs pkgs; };
-    languagePacks = [
-      "es-MX"
-      "es-ES"
-    ];
+  home-manager.users.${usuario} = {
+    programs.firefox = {
+      enable = true;
+      package = (pkgs.wrapFirefox (pkgs.firefox-unwrapped.override { pipewireSupport = true; }) { });
+      profiles = perfiles;
+      languagePacks = [
+        "es-MX"
+        "es-ES"
+      ];
+    };
+
+    xdg.desktopEntries = builtins.listToAttrs (
+      map (clave: {
+        name = "firefox-${clave}";
+        value = {
+          name = "Firefox ${clave}";
+          genericName = "Navegador Web";
+          exec = "firefox -p ${clave}";
+          terminal = false;
+          categories = [
+            "Network"
+            "WebBrowser"
+          ];
+          mimeType = [
+            "text/html"
+            "text/xml"
+          ];
+        };
+      }) (builtins.attrNames perfiles)
+    );
   };
 }
