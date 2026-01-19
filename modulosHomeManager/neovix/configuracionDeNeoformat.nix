5c066a597422c571ba17382ac51a4772960a1aa6
anfigeno <dolorcriticodevastador@proton.me>
2026-01-19 17:18:55 -0500

    Mejoramos la validación de activación

--- modulosHomeManager/neovix/configuracionDeNeoformat.nix
+++ modulosHomeManager/neovix/configuracionDeNeoformat.nix
@@ -6,105 +6,107 @@
 }:
 let
   cfg = config.programs.neovix;
+
+  lenguajesActivadosYConFormateadores =
+    cfg.lenguajes
+    |> lib.mapAttrsToList (
+      clave: lenguaje: with lenguaje; if !activar || formateadores == null then clave else null
+    )
+    |> builtins.filter (x: x != null)
+    |> removeAttrs cfg.lenguajes;
 in
 {
-  programs.neovix.complementos = lib.mkIf (cfg.activar && cfg.formateadores != null) {
-    "Neoformat" =
-      let
-        lenguajesActivadosYConFormateadores =
-          cfg.lenguajes
-          |> lib.mapAttrsToList (
-            clave: lenguaje: with lenguaje; if !activar || formateadores == null then clave else null
-          )
-          |> builtins.filter (x: x != null)
-          |> removeAttrs cfg.lenguajes;
-
-        formateadoresEnUso =
-          lenguajesActivadosYConFormateadores
-          |> lib.mapAttrsToList (_: lenguaje: lenguaje.formateadores)
-          |> lib.lists.flatten
-          |> lib.unique
-          |> (
-            x:
-            cfg.formateadores
-            |> lib.mapAttrsToList (
-              clave: formateador:
-              if !(builtins.elem clave x) then
-                null
-              else
-                {
-                  name = clave;
-                  value = formateador;
-                }
-            )
-          )
-          |> builtins.filter (x: x != null)
-          |> lib.listToAttrs;
-      in
+  programs.neovix.complementos =
+    lib.mkIf (cfg.activar && lenguajesActivadosYConFormateadores != { })
       {
-        paquete = pkgs.vimPlugins.neoformat;
-        dependenciasDeSistema =
-          formateadoresEnUso
-          |> lib.mapAttrsToList (_: formateador: formateador.paquete)
-          |> builtins.filter (x: x != null);
-        configuracion =
+        "Neoformat" =
           let
-            configuracionLuaDeFormateadores =
-              formateadoresEnUso
-              |> lib.mapAttrsToList (
-                clave: formateador:
-                if formateador.configuracion == null then
-                  null
-                else
-                  /* lua */ ''local configuracion_${clave} = ${
-                    formateador.configuracion |> lib.generators.toLua { }
-                  }''
-              )
-              |> builtins.filter (x: x != null)
-              |> builtins.concatStringsSep "\n";
-
-            formateadoresConfiguradosParaCadaLenguaje =
+            formateadoresEnUso =
               lenguajesActivadosYConFormateadores
-              |> lib.mapAttrsToList (
-                clave: lenguaje:
-                lenguaje.formateadores
-                |> map (
-                  nombreDeFormateador:
-                  /* lua */ ''vim.g.neoformat_${clave}_${nombreDeFormateador} = configuracion_${nombreDeFormateador}''
+              |> lib.mapAttrsToList (_: lenguaje: lenguaje.formateadores)
+              |> lib.lists.flatten
+              |> lib.unique
+              |> (
+                x:
+                cfg.formateadores
+                |> lib.mapAttrsToList (
+                  clave: formateador:
+                  if !(builtins.elem clave x) then
+                    null
+                  else
+                    {
+                      name = clave;
+                      value = formateador;
+                    }
                 )
               )
-              |> lib.lists.flatten
-              |> builtins.concatStringsSep "\n";
-
-            formateadoresActivadosParaCajaLenguaje =
-              lenguajesActivadosYConFormateadores
-              |> lib.mapAttrsToList (
-                clave: lenguaje:
-                /* lua */ ''vim.g.neoformat_${clave}_enabled = ${
-                  lenguaje.formateadores |> lib.generators.toLua { multiline = false; }
-                } ''
-              )
-              |> builtins.concatStringsSep "\n";
-
-            configuracion =
-              [
-                configuracionLuaDeFormateadores
-                formateadoresConfiguradosParaCadaLenguaje
-                formateadoresActivadosParaCajaLenguaje
-              ]
-              |> builtins.concatStringsSep "\n\n";
+              |> builtins.filter (x: x != null)
+              |> lib.listToAttrs;
           in
-          configuracion;
+          {
+            paquete = pkgs.vimPlugins.neoformat;
+            dependenciasDeSistema =
+              formateadoresEnUso
+              |> lib.mapAttrsToList (_: formateador: formateador.paquete)
+              |> builtins.filter (x: x != null);
+            configuracion =
+              let
+                configuracionLuaDeFormateadores =
+                  formateadoresEnUso
+                  |> lib.mapAttrsToList (
+                    clave: formateador:
+                    if formateador.configuracion == null then
+                      null
+                    else
+                      /* lua */ ''local configuracion_${clave} = ${
+                        formateador.configuracion |> lib.generators.toLua { }
+                      }''
+                  )
+                  |> builtins.filter (x: x != null)
+                  |> builtins.concatStringsSep "\n";
 
-        lazy = {
-          comandos = [ "Neoformat" ];
-          teclas = {
-            "<s-f>" = {
-              comando = "<cmd>Neoformat<cr>";
-              descripcion = "Formatea el buffer";
+                formateadoresConfiguradosParaCadaLenguaje =
+                  lenguajesActivadosYConFormateadores
+                  |> lib.mapAttrsToList (
+                    clave: lenguaje:
+                    lenguaje.formateadores
+                    |> map (
+                      nombreDeFormateador:
+                      /* lua */ ''vim.g.neoformat_${clave}_${nombreDeFormateador} = configuracion_${nombreDeFormateador}''
+                    )
+                  )
+                  |> lib.lists.flatten
+                  |> builtins.concatStringsSep "\n";
+
+                formateadoresActivadosParaCajaLenguaje =
+                  lenguajesActivadosYConFormateadores
+                  |> lib.mapAttrsToList (
+                    clave: lenguaje:
+                    /* lua */ ''vim.g.neoformat_${clave}_enabled = ${
+                      lenguaje.formateadores |> lib.generators.toLua { multiline = false; }
+                    } ''
+                  )
+                  |> builtins.concatStringsSep "\n";
+
+                configuracion =
+                  [
+                    configuracionLuaDeFormateadores
+                    formateadoresConfiguradosParaCadaLenguaje
+                    formateadoresActivadosParaCajaLenguaje
+                  ]
+                  |> builtins.concatStringsSep "\n\n";
+              in
+              configuracion;
+
+            lazy = {
+              comandos = [ "Neoformat" ];
+              teclas = {
+                "<s-f>" = {
+                  comando = "<cmd>Neoformat<cr>";
+                  descripcion = "Formatea el buffer";
+                };
+              };
             };
           };
-        };
       };
-  };
 }
