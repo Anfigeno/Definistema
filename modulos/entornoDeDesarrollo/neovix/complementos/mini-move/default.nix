6fbca3f428cecf73a017750d3f52f7933643c955
anfigeno <dolorcriticodevastador@proton.me>
2026-01-19 20:58:56 -0500

    Cambio en la carga y configuración

--- modulos/entornoDeDesarrollo/neovix/complementos/mini-move/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/mini-move/default.nix
@@ -5,77 +5,20 @@
     configuracion = /* lua */ ''
       require("mini.move").setup({
         mappings = {
-          left = "",
-          down = "",
-          up = "",
-          right = "",
-          line_left = "",
-          line_down = "",
-          line_up = "",
-          line_right = "",
+          left = '<M-h>',
+          right = '<M-l>',
+          down = '<M-j>',
+          up = '<M-k>',
+          line_left = '<M-h>',
+          line_right = '<M-l>',
+          line_down = '<M-j>',
+          line_up = '<M-k>',
         },
         options = {
           reindent_linewise = true,
         },
       })
     '';
-    lazy.teclas = {
-      "<M-h>" = {
-        accion = /* lua */ ''
-          if vim.fn.mode() == "v" then
-            require("mini.move").move_selection("left")
-          else
-            require("mini.move").move_line("left")
-          end
-        '';
-        descripcion = "Mini Move: Mover a la izquierda";
-        modos = [
-          "v"
-          "n"
-        ];
-      };
-      "<M-j>" = {
-        accion = /* lua */ ''
-          if vim.fn.mode() == "v" then
-            require("mini.move").move_selection("down")
-          else
-            require("mini.move").move_line("down")
-          end
-        '';
-        descripcion = "Mini Move: Mover abajo";
-        modos = [
-          "v"
-          "n"
-        ];
-      };
-      "<M-k>" = {
-        accion = /* lua */ ''
-          if vim.fn.mode() == "v" then
-            require("mini.move").move_selection("up")
-          else
-            require("mini.move").move_line("up")
-          end
-        '';
-        descripcion = "Mini Move: Mover arriba";
-        modos = [
-          "v"
-          "n"
-        ];
-      };
-      "<M-l>" = {
-        accion = /* lua */ ''
-          if vim.fn.mode() == "v" then
-            require("mini.move").move_selection("right")
-          else
-            require("mini.move").move_line("right")
-          end
-        '';
-        descripcion = "Mini Move: Mover a la derecha";
-        modos = [
-          "v"
-          "n"
-        ];
-      };
-    };
+    lazy.eventos = [ "VeryLazy" ];
   };
 }
