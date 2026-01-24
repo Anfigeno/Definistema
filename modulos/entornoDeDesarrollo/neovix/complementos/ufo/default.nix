a5d77197e2cb3d1728a02b4c3aa69cfb81ee51e1
anfigeno <dolorcriticodevastador@proton.me>
2026-01-24 07:55:24 -0500

    Quitamos la linea de pliegues de nvim

--- modulos/entornoDeDesarrollo/neovix/complementos/ufo/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/ufo/default.nix
@@ -7,7 +7,6 @@
       nvim-treesitter
     ];
     configuracion = /* lua */ ''
-      vim.o.foldcolumn = "1"
       vim.o.foldlevel = 99
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
