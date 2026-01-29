f0edea7f02c44aa474a821bbff9e25dacce74148
anfigeno <dolorcriticodevastador@proton.me>
2026-01-28 20:09:49 -0500

    Neovix: Más atajos de teclado para bufferline

--- modulos/entornoDeDesarrollo/neovix/complementos/bufferline/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/bufferline/default.nix
@@ -36,10 +36,6 @@
           '';
           descripcion = "Bufferline: Anterior buffer";
         };
-        "<space>x" = {
-          comando = "<cmd>bp<bar>sp<bar>bn<bar>bd<CR>";
-          descripcion = "Bufferline: Cerrar buffer";
-        };
         "<c-t>" = {
           comando = "<cmd>tabnew<cr>";
           descripcion = "Nueva pestaña";
@@ -47,6 +43,42 @@
         "<m-tab>" = {
           comando = "<cmd>tabnext<cr>";
           descripcion = "Siguiente pestaña";
+        };
+        "<space>bc" = {
+          comando = "<cmd>bp<bar>sp<bar>bn<bar>bd<CR>";
+          descripcion = "Bufferline: Cerrar buffer";
+        };
+        "<space>bCo" = {
+          accion = /* lua */ ''require("bufferline").close_others()'';
+          descripcion = "Bufferline: Cerrar los demás bufferes";
+        };
+        "<space>bCl" = {
+          accion = /* lua */ ''require("bufferline").close_in_direction("right")'';
+          descripcion = "Bufferline: Cerrar los bufferes a la derecha";
+        };
+        "<space>bCh" = {
+          accion = /* lua */ ''require("bufferline").close_in_direction("left")'';
+          descripcion = "Bufferline: Cerrar los bufferes a la izquierda";
+        };
+        "<space>bCe" = {
+          accion = /* lua */ ''require("bufferline").close_with_pick()'';
+          descripcion = "Bufferline: Elegir buffer para cerrar";
+        };
+        "<space>bml" = {
+          accion = /* lua */ ''require("bufferline").move(1)'';
+          descripcion = "Bufferline: Mover buffer a la derecha";
+        };
+        "<space>bmh" = {
+          accion = /* lua */ ''require("bufferline").move(-1)'';
+          descripcion = "Bufferline: Mover buffer a la izquierda";
+        };
+        "<space>be" = {
+          accion = /* lua */ ''require("bufferline").pick()'';
+          descripcion = "Bufferline: Elegir buffer";
+        };
+        "<space>br" = {
+          comando = /* vim */ '':BufferLineTabRename '';
+          descripcion = "Bufferline: Renombrar pestaña";
         };
       };
     };
