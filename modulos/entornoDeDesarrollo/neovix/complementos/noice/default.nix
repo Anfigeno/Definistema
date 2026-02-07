831788c7f9d9ab1d645a8a0a4160e995f9a52cc8
anfigeno <dolorcriticodevastador@proton.me>
2026-02-07 17:59:41 -0500

    Adaptamos noice.nvim al tema mestizo256.nvim

--- modulos/entornoDeDesarrollo/neovix/complementos/noice/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/noice/default.nix
@@ -4,22 +4,17 @@
     paquete = pkgs.vimPlugins.noice-nvim;
     dependencias = with pkgs.vimPlugins; [ nui-nvim ];
     configuracion = /* lua */ ''
-      require("noice").setup({
+      require("noice").setup(require("mestizo256.integraciones_especiales.noice")({
         lsp = {
+          signature = { enabled = true },
+          hover = { enabled = true },
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
           },
         },
-        presets = {
-          bottom_search = true,
-          command_palette = true,
-          long_message_to_split = true,
-          inc_rename = true,
-          lsp_doc_border = false,
-        },
-      })
+      }))
     '';
     lazy.eventos = [ "VeryLazy" ];
   };
