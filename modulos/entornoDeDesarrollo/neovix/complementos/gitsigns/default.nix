6d4967eea241945dc430a8bb0a4bc7e0321b0184
anfigeno <dolorcriticodevastador@proton.me>
2026-02-17 09:35:11 -0500

    Neovix: Configuración de gitsigns

--- modulos/entornoDeDesarrollo/neovix/complementos/gitsigns/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/gitsigns/default.nix
@@ -3,7 +3,14 @@
   home-manager.users.${usuario}.programs.neovix.complementos."Gitsigns" = {
     paquete = pkgs.vimPlugins.gitsigns-nvim;
     configuracion = /* lua */ ''
-      require("gitsigns").setup({ current_line_blame = true })
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "right_align",
+          delay = 1000,
+        },
+      })
     '';
     lazy.eventos = [
       "BufReadPre"
