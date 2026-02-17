8f60925ba296f82a550c7f1b56e6916df8b77d6f
anfigeno <dolorcriticodevastador@proton.me>
2026-02-16 22:06:46 -0500

    Actualizamos el tema mestizo256

--- modulos/entornoDeDesarrollo/neovix/complementos/mestizo256/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/mestizo256/default.nix
@@ -2,7 +2,7 @@
 {
   home-manager.users.${usuario}.programs.neovix.complementos."Mestizo 256" = {
     paquete = util.vim.obtenerComplementoDeGithub {
-      rev = "1947566ba39757344e06b28462fbd9a6291bec8b";
+      rev = "5bd3bb1bc235af7d0a0201d1d34badf86b74757a";
       ref = "main";
       repo = "TemaMestizo/Mestizo256.nvim";
     };
