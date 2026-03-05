0fb7da1e65eb1a08c59b0a2f88b9d00c978423df
anfigeno <dolorcriticodevastador@proton.me>
2026-03-05 15:38:09 -0500

    Configuracion de network manager para preferir wifi sobre ethernet

--- maquinas/l470/configuracion.nix
+++ maquinas/l470/configuracion.nix
@@ -24,4 +24,17 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
+
+  networking.networkmanager.settings = {
+    "connection-wifi" = {
+      "match-device" = "type:wifi";
+      "ipv4.route-metric" = 50;
+      "ipv6.route-metric" = 50;
+    };
+    "connection-ethernet" = {
+      "match-device" = "type:ethernet";
+      "ipv4.route-metric" = 100;
+      "ipv6.route-metric" = 100;
+    };
+  };
 }
