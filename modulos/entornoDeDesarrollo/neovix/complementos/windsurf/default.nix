e94255dcfa6846e8c962b96619442c420bdb3b00
anfigeno <dolorcriticodevastador@proton.me>
2026-03-10 21:48:33 -0500

    Neovix: Desactivamos windsurf

--- modulos/entornoDeDesarrollo/neovix/complementos/windsurf/default.nix
+++ modulos/entornoDeDesarrollo/neovix/complementos/windsurf/default.nix
@@ -1,7 +1,13 @@
-{ pkgs, usuario, ... }:
+{
+  pkgs,
+  usuario,
+  config,
+  ...
+}:
 {
   home-manager.users.${usuario}.programs.neovix.complementos = {
     "Windsurf" = {
+      activar = false;
       paquete = pkgs.vimPlugins.windsurf-nvim;
       dependencias = with pkgs.vimPlugins; [
         plenary-nvim
@@ -13,15 +19,19 @@
       lazy.activar = false;
     };
 
-    "Blink".opts.sources = {
-      default = [ "codeium" ];
-      providers = {
-        codeium = {
-          name = "Codeium";
-          module = "codeium.blink";
-          async = true;
+    "Blink".opts.sources =
+      if !config.home-manager.users.${usuario}.programs.neovix.complementos."Windsurf".activar then
+        { }
+      else
+        {
+          default = [ "codeium" ];
+          providers = {
+            codeium = {
+              name = "Codeium";
+              module = "codeium.blink";
+              async = true;
+            };
+          };
         };
-      };
-    };
   };
 }
